c700b96c9b4c5a077dd979fb4e75984d17cfe4cf
Makson Vinicio <maksonvinicio7@gmail.com>
2023-02-26 15:18:57 -0300

    Criando rotas e toml para configs

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -2,5 +2,154 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/maksonviini/Go-Book-Management-System/pkg/models"
+	"log"
 	"net/http"
+	"strconv"
 )
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	var book models.Book
+
+	err := json.NewDecoder(r.Body).Decode(&book)
+
+	if err != nil {
+		log.Printf("Erro ao fazer decode do json: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	id, err := models.Insert(book)
+
+	var resp map[string]any
+
+	if err != nil {
+		resp = map[string]any{
+			"Error":   true,
+			"Message": fmt.Sprintf("Ocorreu um erro ao tentar inserir: %v", err),
+		}
+	} else {
+		resp = map[string]any{
+			"Error":   false,
+			"Message": fmt.Sprintf("Inserido com sucesso! ID: %d", id),
+		}
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(resp)
+
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+
+	if err != nil {
+		log.Printf("Erro ao fazer parse do id: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	var book models.Book
+
+	err = json.NewDecoder(r.Body).Decode(&book)
+
+	if err != nil {
+		log.Printf("Erro ao fazer decode do json: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	rows, err := models.Update(int64(id), book)
+
+	var resp map[string]any
+
+	if err != nil {
+		log.Printf("Erro ao atualizar registro: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if rows > 1 {
+		log.Printf("Error: Foram atualizados %d registros", rows)
+	}
+
+	resp = map[string]any{
+		"Error":   false,
+		"Message": "Dados atualizados com sucesso!",
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+
+	if err != nil {
+		log.Printf("Erro ao fazer parse do id: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	rows, err := models.Delete(int64(id))
+
+	var resp map[string]any
+
+	if err != nil {
+		log.Printf("Erro ao remover registro: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	if rows > 1 {
+		log.Printf("Error: Foram removidos %d registros", rows)
+	}
+
+	resp = map[string]any{
+		"Error":   false,
+		"Message": "Dados removidos com sucesso!",
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(resp)
+}
+
+func GetAll(w http.ResponseWriter, r *http.Request) {
+	book, err := models.GetAll()
+
+	if err != nil {
+		log.Printf("Erro ao obter registros: %v", err)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(book)
+}
+
+func Get(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+
+	if err != nil {
+		log.Printf("Erro ao fazer parse do id: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	book, err := models.Get(int64(id))
+
+	if err != nil {
+		log.Printf("Erro ao obter registro: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(book)
+}
--- pkg/routes/book-routes.go
+++ pkg/routes/book-routes.go
@@ -1,5 +1,28 @@
 package routes
 
-// import (
-// 	"github.com/maksonviini/Go-Book-Management-System/pkg/controllers"
-// )
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/maksonviini/Go-Book-Management-System/pkg/config"
+	"github.com/maksonviini/Go-Book-Management-System/pkg/controllers"
+)
+
+func main() {
+	err := config.Load()
+
+	if err != nil {
+		panic(err)
+	}
+
+	r := chi.NewRouter()
+
+	r.Post("/", controllers.Create)
+	r.Get("/", controllers.GetAll)
+	r.Put("/{id}", controllers.Update)
+	r.Delete("/{id}", controllers.Delete)
+	r.Get("/{id}", controllers.Get)
+
+	http.ListenAndServe(fmt.Sprint(":%s", config.GetServerPort()), r)
+}
